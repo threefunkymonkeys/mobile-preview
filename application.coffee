cc033201b62f728bada834a56cf965063c7a12b7
Leandro López (inkel) <inkel.ar@gmail.com>
2011-05-17 22:53:56 -0300

    Improved texts, design and functionality

--- application.coffee
+++ application.coffee
@@ -1,10 +1,18 @@
-$(document).ready ->
-  if m = document.location.search.match(/url=([^/&]+)/)
-    url = decodeURIComponent m[1]
-    $("input[name=url]").val(url)
-    $("iframe").attr "src", url
-
-  $("form").submit ->
+window.MobilePreview =
+  onsubmit: ->
     event.preventDefault()
     url = this.elements.url.value
+    MobilePreview.loadSite url
+
+  onload: ->
+    if m = document.location.search.match(/url=([^/&]+)/)
+      url = decodeURIComponent m[1]
+      $("input[name=url]").val(url)
+      MobilePreview.loadSite url
+      $("form").submit MobilePreview.onsubmit
+
+  loadSite: (url) ->
     $('iframe').attr "src", url
+    $("#share").css("visibility", "visible").find("a").attr("href", "?url=" + encodeURIComponent(url))
+
+$(document).ready MobilePreview.onload
--- application.js
+++ application.js
@@ -1,15 +1,24 @@
 (function() {
-  $(document).ready(function() {
-    var m, url;
-    if (m = document.location.search.match(/url=([^/&]+)/)) {
-      url = decodeURIComponent(m[1]);
-      $("input[name=url]").val(url);
-      $("iframe").attr("src", url);
-    }
-    return $("form").submit(function() {
+  window.MobilePreview = {
+    onsubmit: function() {
+      var url;
       event.preventDefault();
       url = this.elements.url.value;
-      return $('iframe').attr("src", url);
-    });
-  });
+      return MobilePreview.loadSite(url);
+    },
+    onload: function() {
+      var m, url;
+      if (m = document.location.search.match(/url=([^/&]+)/)) {
+        url = decodeURIComponent(m[1]);
+        $("input[name=url]").val(url);
+        MobilePreview.loadSite(url);
+        return $("form").submit(MobilePreview.onsubmit);
+      }
+    },
+    loadSite: function(url) {
+      $('iframe').attr("src", url);
+      return $("#share").css("visibility", "visible").find("a").attr("href", "?url=" + encodeURIComponent(url));
+    }
+  };
+  $(document).ready(MobilePreview.onload);
 }).call(this);

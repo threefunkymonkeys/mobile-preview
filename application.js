a2825d8f285f45f75c4d74be2b62d13ddf6ab6cc
Lautaro Orazi <orazile@gmail.com>
2011-05-18 12:21:02 -0300

    issue #3 Indicate devices to use for preview, build share link

--- application.js
+++ application.js
@@ -1,24 +1,30 @@
 (function() {
   window.MobilePreview = {
     onsubmit: function() {
-      var url;
-      event.preventDefault();
-      url = this.elements.url.value;
-      return MobilePreview.loadSite(url);
     },
     onload: function() {
-      var m, url;
+      var m, c, url,share="";
       if (m = document.location.search.match(/url=([^/&]+)/)) {
         url = decodeURIComponent(m[1]);
         $("input[name=url]").val(url);
-        MobilePreview.loadSite(url);
-        return $("form").submit(MobilePreview.onsubmit);
+        if(c = document.location.search.match(/(ipp|ipl|ipadp|ipadl)/g)) {
+          $(c).each(function(){ $("input[name="+this+"]").attr("checked",true) });
+          console.log(c = document.location.search.match(/url=[^/&]+(.*)/));
+          share = c[1];
+        }
+        MobilePreview.loadSite(url, share);
       }
     },
-    loadSite: function(url) {
-      $('iframe').attr("src", url);
-      return $("#share").css("visibility", "visible").find("a").attr("href", "?url=" + encodeURIComponent(url));
+    loadSite: function(url, addShare) {
+      var checkBlock = $("#checks-block");
+      $(".ipad, .iphone").hide();
+      checkBlock.find("input:checked").each(function(){
+        var value = $(this).val();
+        $("#"+value).attr("src", url).parent().show();
+      });
+      $("#share").css("visibility", "visible").find("a").attr("href", "?url=" + encodeURIComponent(url) + addShare);
     }
   };
+
   $(document).ready(MobilePreview.onload);
 }).call(this);
